b78f4deaf4bb9ab0031e62cfbb806f9e34515f91
ssmirnovacode <svetlana.smirnova213@gmail.com>
2023-03-07 11:17:21 +0100

    CSS imports from packages enabled

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # IDE Doc
 
 ## Dynamic npm-packages fetching
-Using a customised plugin for ESBuild that intercepts int onLoad event so that instead of looking for a package in file system it is fetched directly from NPM (unpkg.io, that gives us direct access to NPM content without CORS issues)
+Using a customised plugin for ESBuild that intercepts int onLoad event so that instead of looking for a package in file system it is fetched directly from NPM (unpkg.io, that gives us direct access to NPM content without CORS issues).
+
+CSS: ESBuild requires an output file for CSS bundle, and we don't have access to the file system. Therefore, we wrapped CSS content into JS and inject it to the code.
 
 ## Cacheing
 As we fetch NPM packages for every import, there can be a lot of requests. To handle it better, we are using IndexedDB via localforage library and saving the configuration objects for fetched packages there.
--- src/plugins/fetchPlugin.ts
+++ src/plugins/fetchPlugin.ts
@@ -29,10 +29,21 @@ export const fetchPlugin = (inputCode: string) => {
         
                 const { data, request } = await axios.get(args.path);
             
+                const fileType = args.path.match(/.css$/) ? 'css' : 'jsx';
+
+                const escapedCss = data.replace(/\n/g, '').replace(/"/g, '\\"').replace(/'/g, "\\'");
+
+                const contents = fileType === 'css' ?
+                ` 
+                    const style = document.createElement('style');
+                    style.innerText = '${escapedCss}';
+                    document.head.appendChild(style);
+                ` : data;
+
                 const result: esbuild.OnLoadResult = {
-                loader: 'jsx',
-                contents: data,
-                resolveDir: new URL('./', request.responseURL).pathname // describes the path where we found the original file
+                    loader: 'jsx',
+                    contents,
+                    resolveDir: new URL('./', request.responseURL).pathname // describes the path where we found the original file
                 };
         
                 await fileCache.setItem(args.path, result);
