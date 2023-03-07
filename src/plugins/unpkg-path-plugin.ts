5800811f6e73e0804d5e543db1dc3dffbce52ecf
ssmirnovacode <svetlana.smirnova213@gmail.com>
2023-03-07 09:58:49 +0100

    Caching with IndexedDB lib enabled for dynamic imports

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,6 +1,11 @@
 import * as esbuild from 'esbuild-wasm';
 import axios from 'axios';
+import localForage from 'localforage';
  
+const fileCache = localForage.createInstance({
+  name: 'filecache'
+});
+
 export const unpkgPathPlugin = () => {
   return {
     name: 'unpkg-path-plugin', // just for debugging
@@ -41,13 +46,23 @@ export const unpkgPathPlugin = () => {
           };
         } 
 
+        const cachedData = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
+
+        if (cachedData) {
+          return cachedData;
+        }
+
         const { data, request } = await axios.get(args.path);
        
-        return {
+        const result: esbuild.OnLoadResult = {
           loader: 'jsx',
           contents: data,
           resolveDir: new URL('./', request.responseURL).pathname // describes the path where we found the original file
         };
+
+        await fileCache.setItem(args.path, result);
+
+        return result;
       });
     },
   };
