6280b14325eef81a31bca83c7d3fb83feb671164
ssmirnovacode <svetlana.smirnova213@gmail.com>
2023-03-11 11:09:33 +0100

    Code editor instance refactored to CodeCell component for multiplying

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
-import React, {useState } from 'react';
-import bundle from './bundler';
-import CodeEditor from './components/code-editor';
+import React from 'react';
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
-import Preview from './components/Rreview';
+import CodeCell from './components/CodeCell';
 
-function App() {
-  
-  const [ input, setInput] = useState('');
-  const [ code, setCode ] = useState('')
-
-  const handleClick = async () => {
-    const output = await bundle(input);
-    setCode(output);
-  }
+const App = () => {
 
   return (
     <div >
-      <CodeEditor initialValue={"const a = 1;"} onChange={value => setInput(value)} />
-     <div>
-      <button onClick={handleClick}>Submit</button>
-     </div>
-     <Preview code={code} />
+      <CodeCell />
+      <CodeCell />
     </div>
   );
 }
---
+++ src/components/CodeCell.tsx
@@ -0,0 +1,28 @@
+import React, {useState } from 'react';
+import bundle from '../bundler';
+import CodeEditor from './code-editor';
+import Preview from './Rreview';
+
+// a unit of multiple code editor windows
+const CodeCell = () => {
+  
+  const [ input, setInput] = useState('');
+  const [ code, setCode ] = useState('')
+
+  const handleClick = async () => {
+    const output = await bundle(input);
+    setCode(output);
+  }
+
+  return (
+    <div >
+      <CodeEditor initialValue={"const a = 1;"} onChange={value => setInput(value)} />
+     <div>
+      <button onClick={handleClick}>Submit</button>
+     </div>
+     <Preview code={code} />
+    </div>
+  );
+}
+
+export default CodeCell;
