5177f521776793c799e42edd0ce6f82a764acd89
ssmirnovacode <svetlana.smirnova213@gmail.com>
2023-03-11 11:03:03 +0100

    Refactor: isolating bundle logic to bundler folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,17 @@
-import React, { useEffect, useRef, useState } from 'react';
-import * as esbuild from 'esbuild-wasm';
-import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
-import { fetchPlugin } from './plugins/fetchPlugin';
+import React, {useState } from 'react';
+import bundle from './bundler';
 import CodeEditor from './components/code-editor';
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import Preview from './components/Rreview';
 
 function App() {
-
-  const ref = useRef<any>()
   
   const [ input, setInput] = useState('');
   const [ code, setCode ] = useState('')
 
-  useEffect(() => {
-    startService();
-  }, [])
-
-  const startService = async() => {
-    ref.current = await esbuild.startService({
-      worker: true,
-      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm' 
-    })
-  }
-
   const handleClick = async () => {
-    if (!ref.current) return;
-    
-
-    //transpiling:
-    const result = await ref.current.build({
-      entryPoints: ['index.js'],
-      bundle: true,
-      write: false,
-      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
-      define: {
-        'process.env.NODE_ENV': '"production"',
-        global: 'window'
-      }
-    })
-    setCode(result.outputFiles[0].text)
+    const output = await bundle(input);
+    setCode(output);
   }
 
   return (
