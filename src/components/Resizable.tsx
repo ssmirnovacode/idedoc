f4692965ed6f8605683255ed6142fca0dd6db064
ssmirnovacode <svetlana.smirnova213@gmail.com>
2023-03-12 18:46:54 +0100

    Debouncing added for resize event not to rerender on each px resized

--- src/components/Resizable.tsx
+++ src/components/Resizable.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { ResizableBox, ResizableBoxProps } from "react-resizable";
 import './Resizable.css';
 
@@ -8,12 +9,33 @@ interface ResizableProps {
 
 const Resizable = (props: ResizableProps) => {
     const { direction, children } = props;
+
+    const [ innerHeight, setInnerHeight ] = useState(window.innerHeight);
+    const [ innerWidth, setInnerWidth ] = useState(window.innerWidth);
+
+    useEffect(() => {
+        let timer: any;
+        const listener = () => {
+            if (timer) clearTimeout(timer); 
+            timer = setTimeout(() => { // debouncing
+                setInnerHeight(window.innerHeight);
+                setInnerWidth(window.innerWidth)
+            }, 100)
+            
+        }
+        window.addEventListener('resize', listener);
+
+        return () => {
+            window.removeEventListener('resize', listener)
+        }
+    }, [])
+
     const getResizableProps = (): ResizableBoxProps => {
         if (direction === 'vertical') {
             return {
                 resizeHandles: ['s'],
                 minConstraints: [Infinity, 24],
-                maxConstraints:[Infinity, window.innerHeight * 0.9], 
+                maxConstraints:[Infinity, innerHeight * 0.9], 
                 height: 300 ,
                 width: Infinity
             }
@@ -21,10 +43,10 @@ const Resizable = (props: ResizableProps) => {
         else return {
             className: 'resize-horizontal',
             resizeHandles: ['e'],
-            minConstraints: [window.innerWidth * 0.2, Infinity],
-            maxConstraints:[window.innerWidth * 0.75, Infinity], 
+            minConstraints: [innerWidth * 0.2, Infinity],
+            maxConstraints:[innerWidth * 0.75, Infinity], 
             height: Infinity,
-            width: window.innerWidth * 0.75
+            width: innerWidth * 0.75
         }
     }
 
