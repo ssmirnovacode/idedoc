daecbac1ff7b1d7a3883b6d04fcb9279b5b736dc
ssmirnovacode <svetlana.smirnova213@gmail.com>
2023-03-12 18:02:39 +0100

    Horizontal resizing enabled

--- src/components/Resizable.tsx
+++ src/components/Resizable.tsx
@@ -1,4 +1,4 @@
-import { ResizableBox } from "react-resizable";
+import { ResizableBox, ResizableBoxProps } from "react-resizable";
 import './Resizable.css';
 
 interface ResizableProps {
@@ -8,14 +8,28 @@ interface ResizableProps {
 
 const Resizable = (props: ResizableProps) => {
     const { direction, children } = props;
+    const getResizableProps = (): ResizableBoxProps => {
+        if (direction === 'vertical') {
+            return {
+                resizeHandles: ['s'],
+                minConstraints: [Infinity, 24],
+                maxConstraints:[Infinity, window.innerHeight * 0.9], 
+                height: 300 ,
+                width: Infinity
+            }
+        }
+        else return {
+            className: 'resize-horizontal',
+            resizeHandles: ['e'],
+            minConstraints: [window.innerWidth * 0.2, Infinity],
+            maxConstraints:[window.innerWidth * 0.75, Infinity], 
+            height: Infinity,
+            width: window.innerWidth * 0.75
+        }
+    }
 
     return (
-        <ResizableBox 
-            resizeHandles={['s']} // handle in the bottom of the box
-            minConstraints={[Infinity, 24]} // limiting min size to 24px
-            maxConstraints={[Infinity, window.innerHeight * 0.9]} // limiting resizing range (so it doesnt excape outside viewport)
-            height={300} 
-            width={Infinity}>
+        <ResizableBox {...getResizableProps()}>
                 {children}
         </ResizableBox>
     )
