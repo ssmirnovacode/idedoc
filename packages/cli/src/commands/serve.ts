eb691df9e43aada72b3f3956954cb54c21b207b5
ssmirnovacode <svetlana.smirnova213@gmail.com>
2023-03-25 14:44:45 +0100

    CLI basic serve command enabled with local-api

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -1,9 +1,12 @@
+import path from 'path';
 import { Command } from 'commander';
+import { serve } from 'local-api';
 
 export const serveCommand = new Command()
     .command('serve [filename]') // [] mean optional value
     .description('Open a file for editing')
     .option('-p, --port <number>', 'port to run server on', '4005') // <> mean required value
-    .action((filename='notebook.js', options) => { // what happens on command 'serve'
-        console.log(filename, options)
+    .action((filename='notebook.js', options: { port: string }) => { // what happens on command 'serve'
+        const dir = path.join(process.cwd(), path.dirname(filename));
+        serve(+options.port, path.basename(filename), dir)
     })
--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,3 +1,5 @@
-export default () => {
-    console.log('Server is watching u')
+export const serve = (port: number, filename: string, dir: string) => { // *dir
+    console.log(`Serving traffic on port ${port}, saving cells from ${filename} inside directory ${dir}`)
 }
+
+// * dir is where the file is (should be) located (user might type the file with path like 'workspace/notes/notebook.js')
