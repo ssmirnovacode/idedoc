fc3dbe93a88b00f4d369d0daeaac405fb82c63dd
ssmirnovacode <svetlana.smirnova213@gmail.com>
2023-03-25 12:10:51 +0100

    TS configured in local-api, React downgraded in local-client

--- packages/local-client/src/index.tsx
+++ packages/local-client/src/index.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(
+ReactDOM.render(<React.StrictMode>
+  <App />
+</React.StrictMode>,
   document.getElementById('root') as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
