72e6192d94bf3a3422504ce71a40fd880f59fc8d
ssmirnovacode <svetlana.smirnova213@gmail.com>
2023-03-25 15:30:34 +0100

    Proxy enabled for 3000

--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,8 +1,16 @@
-import express from 'express'
+import express from 'express';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 export const serve = (port: number, filename: string, dir: string) => { // *dir
 
    const app = express();
+
+   //We are accessing our local-api on 4005 (dafault), but the request is being automatically proxied through the port where our React app is running (3000).
+   app.use(createProxyMiddleware({
+    target: 'http://localhost:3000',
+    ws: true, // websocket support - default CRA behavior
+    logLevel: 'silent'
+   }))
 
    return new Promise<void>((resolve, reject) => {
     app.listen(port, resolve).on('error', reject)
