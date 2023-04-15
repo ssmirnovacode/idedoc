51f5698c49b55c71f5a8228e9209627e3a2072f3
ssmirnovacode <svetlana.smirnova213@gmail.com>
2023-04-15 13:12:25 +0200

    package names refactored

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -1,37 +1,41 @@
-import path from 'path';
-import { Command } from 'commander';
-import { serve } from 'local-api';
+import path from "path";
+import { Command } from "commander";
+import { serve } from "@idedoc/local-api";
 
-const isProduction = process.env.NODE_ENV === 'production';
+const isProduction = process.env.NODE_ENV === "production";
 
 interface LocalApiError {
-    code: string;
-  }
+  code: string;
+}
 
 export const serveCommand = new Command()
-    .command('serve [filename]') // [] mean optional value
-    .description('Open a file for editing')
-    .option('-p, --port <number>', 'port to run server on', '4005') // <> mean required value
-    .action(async (filename='notebook.js', options: { port: string }) => { // what happens on command 'serve'
-        
-        const isLocalApiError = (err: any): err is LocalApiError => {
-            return typeof err.code === "string";
-          };
-        
-        try {
-            const dir = path.join(process.cwd(), path.dirname(filename));
-            await serve(+options.port, path.basename(filename), dir, !isProduction);
-            console.log(`Opened ${filename}. Navigate to http://localhost:${options.port} to edit the file`)
-        } catch (err) {
-            if (isLocalApiError(err)) {
-              if (err.code === "EADDRINUSE") {
-                console.error("Port is in use. Try running on a different port. You can do it by adding -p <port number> command");
-              }
-            } else if (err instanceof Error) {
-              console.log("Heres the problem: ", err.message);
-            }
-            // in case we failed to start our server:
-            process.exit(1);
-          }
-       
-    })
+  .command("serve [filename]") // [] mean optional value
+  .description("Open a file for editing")
+  .option("-p, --port <number>", "port to run server on", "4005") // <> mean required value
+  .action(async (filename = "notebook.js", options: { port: string }) => {
+    // what happens on command 'serve'
+
+    const isLocalApiError = (err: any): err is LocalApiError => {
+      return typeof err.code === "string";
+    };
+
+    try {
+      const dir = path.join(process.cwd(), path.dirname(filename));
+      await serve(+options.port, path.basename(filename), dir, !isProduction);
+      console.log(
+        `Opened ${filename}. Navigate to http://localhost:${options.port} to edit the file`
+      );
+    } catch (err) {
+      if (isLocalApiError(err)) {
+        if (err.code === "EADDRINUSE") {
+          console.error(
+            "Port is in use. Try running on a different port. You can do it by adding -p <port number> command"
+          );
+        }
+      } else if (err instanceof Error) {
+        console.log("Heres the problem: ", err.message);
+      }
+      // in case we failed to start our server:
+      process.exit(1);
+    }
+  });
--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -25,7 +25,9 @@ export const serve = (
       })
     );
   } else {
-    const packagePath = require.resolve("local-client/build/index.html");
+    const packagePath = require.resolve(
+      "@idedoc/local-client/build/index.html"
+    );
     app.use(express.static(path.dirname(packagePath)));
   }
 
